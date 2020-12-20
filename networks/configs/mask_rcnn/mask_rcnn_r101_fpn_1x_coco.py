35e179d760aca51c25815900e8197940fe7c6dfe
Vzc <452849062@qq.com>
2020-12-20 16:53:06 +0800

    delete test directory

--- networks/configs/mask_rcnn/mask_rcnn_r101_fpn_1x_coco.py
+++
@@ -1,4 +0,0 @@
-_base_ = './mask_rcnn_r50_fpn_1x_coco.py'
-# model = dict(pretrained='torchvision://resnet101', backbone=dict(depth=101))
-model = dict(pretrained='https://download.pytorch.org/models/densenet121-a639ec97.pth',
-             backbone=dict(depth=121,type='DenseNet',num_stages=4))
